8f1525ce919246710da39a0686752dacbc8c0c74
rpanuga1 <rpanuga1@uncc.edu>
2015-11-04 22:23:57 +0000

    Working on Heroku, no movies loading3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,4 @@
 class MoviesController < ApplicationController
-  helper_method :hilight
-  helper_method :chosen_rating?
 
   def show
     id = params[:id] # retrieve movie ID from URI route
@@ -9,27 +7,58 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @all_ratings = ['G','PG','PG-13','R']
-    session[:ratings] = params[:ratings] unless params[:ratings].nil?
-    session[:order] = params[:order] unless params[:order].nil?
-
-    if (params[:ratings].nil? && !session[:ratings].nil?) || (params[:order].nil? && !session[:order].nil?)
-      redirect_to movies_path("ratings" => session[:ratings], "order" => session[:order])
-    elsif !params[:ratings].nil? || !params[:order].nil?
-      if !params[:ratings].nil?
-        array_ratings = params[:ratings].keys
-        return @movies = Movie.where(rating: array_ratings).order(session[:order])
-      else
-        return @movies = Movie.all.order(session[:order])
-      end
-    elsif !session[:ratings].nil? || !session[:order].nil?
-      redirect_to movies_path("ratings" => session[:ratings], "order" => session[:order])
-    else
-      return @movies = Movie.all
+    
+ #    params.each { |p|
+ #       session[p] = p
+ #    }
+#      redirect_to movies_path(params => session)
+#    end
+    
+    @movies = Movie.all
+    @redirect = 0
+    if(@checked != nil)
+      @movies = @movies.find_all{ |m| @checked.has_key?(m.rating) and  @checked[m.rating]==true}      
     end
+    
+    
+   if(params[:sort].to_s == 'title')
+    session[:sort] = params[:sort]
+    @movies = @movies.sort_by{|m| m.title }
+   elsif(params[:sort].to_s == 'release')
+    session[:sort] = params[:sort]
+    @movies = @movies.sort_by{|m| m.release_date.to_s }
+   elsif(session.has_key?(:sort) )
+    params[:sort] = session[:sort]
+    @redirect = 1
+   end
+    
+    
+    if(params[:ratings] != nil)
+      session[:ratings] = params[:ratings]
+      @movies = @movies.find_all{ |m| params[:ratings].has_key?(m.rating) }
+    elsif(session.has_key?(:ratings) )
+      params[:ratings] = session[:ratings]
+      @redirect =1
+    end
+    
+    if(@redirect ==1)
+    redirect_to movies_path(:sort=>params[:sort], :ratings =>params[:ratings] )
+    end
+
+    @checked = {}
+    @all_ratings =  ['G','PG','PG-13','R']
+
+    @all_ratings.each { |rating|
+      if params[:ratings] == nil
+        @checked[rating] = false
+      else
+        @checked[rating] = params[:ratings].has_key?(rating)
+      end
+    }
   end
 
   def new
+    # default: render 'new' template
   end
 
   def create
@@ -56,17 +85,4 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
-  def hilight(column)
-    if(session[:order].to_s == column)
-      return 'hilite'
-    else
-      return nil
-    end
-  end
-
-  def chosen_rating?(rating)
-    chosen_ratings = session[:ratings]
-    return true if chosen_ratings.nil?
-    chosen_ratings.include? rating
-  end
 end
